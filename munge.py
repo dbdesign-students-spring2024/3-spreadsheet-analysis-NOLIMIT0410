88a4e8f523e60aa5a85e133b433dd657bd45fb9b
NOLIMIT0410 <ys4679@nyu.edu>
2024-02-19 23:52:21 -0500

    Finally

--- munge.py
+++ munge.py
@@ -7,9 +7,9 @@ url = "https://data.cityofnewyork.us/resource/k397-673e.json?$limit=5000"
 response = requests.get(url)
 data = json.loads(response.content)
 df = pandas.json_normalize(data)
-df.to_csv("data/original_data_file.csv")
-#clean out unnecessary columns in the dataset
+df.to_csv("data/Original_data_file.csv")
 
+#clean out unnecessary columns in the dataset
 import pandas as pd
 df2 = pd.read_csv("data/original_data_file.csv")
 
@@ -29,4 +29,4 @@ print(df2.head())
 df2.drop(df2[df2['pay_basis'] != 'per Annum'].index, axis=0, inplace=True)
 
 # Save the modified DataFrame back to a CSV file
-df2.to_csv('data/clean_data.csv', index=False)
+df2.to_csv('data/Clean_data.csv', index=False)
